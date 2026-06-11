55794b9aac7cbf63d6c3986958bec310921592b6
AMIKO <akozmanishvili@gmail.com>
2026-06-11 13:44:14 +0400

    Add feature: transaction list appears to DOM

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log("Budget tracker connected");
+//to store transaction objects
+let transactions = [];
+
+//get html elements
+const form = document.getElementById("transaction-form");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
+const transactionList = document.getElementById(`transaction-list`);
+
+//function to add transaction in transaction list
+const addTransactionDOM = function (transaction) {
+  const list = document.createElement("li");
+  list.innerHTML = `${transaction.textInput}: <span>${transaction.amountInput > 0 ? "+$" + transaction.amountInput : "-$" + String(transaction.amountInput).slice(1)} </span>`;
+  transaction.amountInput > 0
+    ? list.classList.add("plus")
+    : list.classList.add("minus");
+  transactionList.appendChild(list);
+};
+
+form.addEventListener("submit", function addTransaction(e) {
+  e.preventDefault();
+  if (text.value.trim() === "" || amount.value.trim() === "") {
+    alert("Enter the transaction");
+    return;
+  }
+
+  const transaction = {
+    id: Date.now(),
+    textInput: text.value,
+    amountInput: Number(amount.value),
+  };
+
+  transactions.push(transaction);
+  addTransactionDOM(transaction);
+  amount.value = "";
+  text.value = "";
+});
